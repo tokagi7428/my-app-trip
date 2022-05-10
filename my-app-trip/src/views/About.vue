683c56f1802283e5291c0ecbf4ce4e99af0b3fc2
tokagi7428 <79756474+tokagi7428@users.noreply.github.com>
2022-05-10 20:02:51 +0700

    some about

--- my-app-trip/src/views/About.vue
+++ my-app-trip/src/views/About.vue
@@ -1,49 +1,80 @@
 <template>
   <div class="about">
-     <div class="about-banner">
-       <img src="https://cdn.pixabay.com/photo/2017/05/23/19/42/seal-2338306__340.png" alt="" class="about-img">
-       <h1 class="about-text">Who we are?</h1>
-     </div>
+    <div class="about-banner">
+      <img
+        src="https://cdn.pixabay.com/photo/2014/08/15/06/15/imprint-418593__340.jpg"
+        alt=""
+        class="about-img"
+      />
+    </div>
+    <div class="about-box">
+      <h1 class="about-header">Who we are ?</h1>
+      <nav class="about-title">
+        <div class="box box1">
+          <h1>Header</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, corporis!</p>
+        </div>
+        <div class="box box2">
+          <h1>Header</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, corporis!</p>
+        </div>
+        <div class="box box3">
+          <h1>Header</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, corporis!</p>
+        </div>
+        <div class="box box4">
+          <h1>Header</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, corporis!</p>
+        </div>
+        <div class="box box5">
+          <h1>Header</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, corporis!</p>
+        </div>
+        <div class="box box6">
+          <h1>Header</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, corporis!</p>
+        </div>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-    name: 'aboutVue',
-    component: {
-      
-    }
-}
+  name: "aboutVue",
+  component: {},
+};
 </script>
 
 <style scoped>
-.about{
-  height: 100vh;
-}
-.about-img{
+
+.about-img {
   width: 100%;
   height: 500px;
 }
-.about-banner{
+
+.about-header {
+  text-align: center;
+  margin: 70px auto;
+}
+.box {
+  background: green;
+  width: 300px;
+  height: 300px;
   position: relative;
   text-align: center;
-  color: #000;
 }
-.about-text{
+.box p{
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  display: block;
+  bottom: 0px;
 }
-@media screen and (max-width: 1280px)  {
-  .about-text{
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.about-title {
+  width: 80%;
+  margin: 50px auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
 }
-};
-  
 </style>
 
